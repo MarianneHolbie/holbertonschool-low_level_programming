ec49d71339d690ac785a9789529d0eacb26d9410
Arrue Marianne <5608@holbertonstudents.com>
2022-09-29 03:34:09 -0700

    first message

---
+++ hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	/*string initialise*/
+	char Reflexion[]= "Programming is like building a multilingual puzzle";
+
+	/*writte the sentence*/
+	puts(Reflexion);
+	return 0;
+}

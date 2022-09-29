746a03b5087f4cdaeba16c021daf3aa2591adffb
Arrue Marianne <5608@holbertonstudents.com>
2022-09-29 03:42:45 -0700

    bugfix quote

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
-int main()
+int main(void)
 {
 	/*string initialise*/
-	char Reflexion[]= "Programming is like building a multilingual puzzle";
+	char Reflexion[] = "\"Programming is like building a multilingual puzzle\"";
 
 	/*writte the sentence*/
 	puts(Reflexion);
-	return 0;
+	return (0);
 }

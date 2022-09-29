e7dfde781e4abc654f79647c7562b233fcedc7ba
Arrue Marianne <5608@holbertonstudents.com>
2022-09-29 06:14:22 -0700

    size

---
+++ hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main- entry point
+ * description : print size various types
+ * Return: 0
+ */
+int main(void)
+{
+	/*Description : use of command printf size of variable */
+	printf("Size of a char: %zu byte(s)\n");
+	printf("Size of an int: %zu byte(s)\n");
+	printf("Size of a long int: %zu byte(s)\n");
+	printf("Size of a long long int: %zu byte(s)\n");
+	printf("Size of a float: %zu byte(s)\n");
+
+	return (0);
+}

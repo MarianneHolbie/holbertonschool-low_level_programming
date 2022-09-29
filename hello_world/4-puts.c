882fac19bdda35495f6003b036d43e0ca6454de9
Arrue Marianne <5608@holbertonstudents.com>
2022-09-29 03:58:33 -0700

    betty style bugfix

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,15 @@
 #include<stdio.h>
+/**
+ * main - Entry point
+ * Description 'write a sentence with puts fonction'
+ * Return: Always 0
+ */
 int main(void)
-{
-	/*string initialise*/
-	char Reflexion[] = "\"Programming is like building a multilingual puzzle";
+	{
+	/**
+	 * Description : Write a sentence with puts
+	 */
+	puts("\"Programming is like building a multilingual puzzle");
 
-	/*writte the sentence*/
-	puts(Reflexion);
 	return (0);
 }

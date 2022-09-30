7169aa93150314963be52279f43ab850cba17775
Arrue Marianne <5608@holbertonstudents.com>
2022-09-30 04:18:10 -0700

    print exactly

---
+++ hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main- Entry point
+ * description: print exactly
+ * return : 1
+ */
+int main(void)
+{
+	/* Description:prints exactly */
+
+	printf("and that piece of art is useful\" - Dora Korpa, 2015-10-19\n");
+
+	return (1);
+}

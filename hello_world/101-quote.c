96d254bb050594612724932cf272286f5bbb9baa
Arrue Marianne <5608@holbertonstudents.com>
2022-09-30 04:22:25 -0700

    bugfix

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -2,13 +2,13 @@
 /**
  * main- Entry point
  * description: print exactly
- * return : 1
+ * Return: 1
  */
 int main(void)
 {
-	/* Description:prints exactly */
+	/* description:prints exactly */
 
-	printf("and that piece of art is useful\" - Dora Korpa, 2015-10-19\n");
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 
 	return (1);
 }

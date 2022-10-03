f04dbf2fa94a0b986b6ba8eb6db4502e5d1b48ad
Arrue Marianne <5608@holbertonstudents.com>
2022-10-03 05:18:09 -0700

    reverse ascii alphabet

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main- Entry point
+ * Description: reverse alphabet
+ * Return: 0
+*/
+int main(void)
+{
+	int ascii;
+
+	/* reverse loop to print reverse alphabet */
+	ascii = 122;
+		while (ascii == 96)
+		{
+			putchar(ascii);
+			ascii--;
+		}
+
+	putchar('\n');
+	return (0);
+}

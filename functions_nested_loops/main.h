c4250a4d26c5de2ca48e899db1c1ff2dbff0a4a7
Arrue Marianne <5608@holbertonstudents.com>
2022-10-04 04:37:03 -0700

    add main.h

---
+++ functions_nested_loops/main.h
@@ -0,0 +1,29 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+*/
+int _putchar(char c)
+{
+		return (write(1, &c, 1));
+}
+
+/** 
+ * print_alphabet- print the alphabet in lowercase
+ * description: alphabet lowercase
+ * Return: Always 0
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter);
+	}
+	_putchar('\n');
+	return (0);
+}

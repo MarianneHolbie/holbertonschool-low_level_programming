abf5a38d81ac81264887290026f9d65013812737
Arrue Marianne <5608@holbertonstudents.com>
2022-10-04 12:43:45 -0700

    bugfix for negativ number

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -2,17 +2,27 @@
 
 /**
  * print_last_digit- function that print last digit of a number
- * @n: integer
- * Description: programm to print last digit
- * Return: 0
+ * @n: integer to extract the last digit
+ * Return: value of the last digit
  */
 
 int print_last_digit(int n)
 {
+	/* u valeur of last digit */
 	int u;
 
-	u = n % 10;
-	_putchar(u);
+	/* % possible only with positiv number */
+	if (n > 0)
+	{
+		u = n % 10;
+		_putchar(u + '0');
+	}
+	else
+	{
+		n = -n;
+		u = n % 10;
+		_putchar(u + '0');
+	}
 	return (0);
 }
 

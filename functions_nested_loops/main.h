17ed7caf676165b088a51e02a16770e7039d7aa8
Arrue Marianne <5608@holbertonstudents.com>
2022-10-04 11:42:09 -0700

    return sign of a number

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -10,3 +10,5 @@ void print_alphabet_x10(void);
 int _islower(int c);
 /* function that checks for alphabetic character */
 int _isalpha(int c);
+/* function that prints the sign of a number */
+int print_sign(int n);

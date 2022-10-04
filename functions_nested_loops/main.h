7bc327c8d387b16c83e11473806e7a9dd7b2234f
Arrue Marianne <5608@holbertonstudents.com>
2022-10-04 07:08:05 -0700

    checking lowercase

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -6,4 +6,5 @@ int _putchar(char);
 void print_alphabet(void);
 /* function print_alphabet_x10 */
 void print_alphabet_x10(void);
-
+/* function that checks for lowercase character */
+int _islower(int c);

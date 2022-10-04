25c32a5ebf484b12e8bab9321a2424600b209e0d
Arrue Marianne <5608@holbertonstudents.com>
2022-10-04 11:14:06 -0700

    program that checks for alphabetic character

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -8,3 +8,5 @@ void print_alphabet(void);
 void print_alphabet_x10(void);
 /* function that checks for lowercase character */
 int _islower(int c);
+/* function that checks for alphabetic character */
+int _isalpha(int c);

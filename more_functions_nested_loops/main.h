6bac3fc2349560ffdd24ef697b1549ff462b8054
Arrue Marianne <5608@holbertonstudents.com>
2022-10-06 12:35:35 -0700

    print number from 0 to 9 without 2 and 4

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -10,4 +10,6 @@ int _isdigit(int c);
 int mul(int a, int b);
 /* function to print numbers from 0 to 9 */
 void print_numbers(void);
+/* function to print numbers from 0 to 9 except 2 and 4 */
+void print_most_numbers(void);
 

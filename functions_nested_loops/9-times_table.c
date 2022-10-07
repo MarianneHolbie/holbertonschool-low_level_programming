0697a4fe5b514729e0fb5e7a1110af51494fa338
Arrue Marianne <5608@holbertonstudents.com>
2022-10-07 06:23:55 -0700

    inverse reflexion !

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -10,44 +10,31 @@ void times_table(void)
 {
 	int a, b, r;
 
-	a = 0;
-	b = 0;
-
-	while (a <= 9)
+	for (a = 0; a < 10; a++)
 	{
-		while (b <= 9)
+		for (b = 0; b < 10; b++)
 		{
 			r = a * b;
-			if (b <= 9)
+			if (r == 0)
 			{
-				if (r >= 10)
-				{
-					_putchar(r / 10 + '0');
-					_putchar(r % 10 + '0');
-				}
-				else
-				{
-					_putchar(r + '0');
-				}
-					if (b != 9)
-					{
-						if (r < 10)
-						{
-							_putchar(',');
-							_putchar(' ');
-							_putchar(' ');
-						}
-						else
-						{
-							_putchar(',');
-							_putchar(' ');
-						}
-					}
-			}	
-			b++;
+				_putchar(r + '0');
+			}
+			if ((r < 10) && (r != 0))
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(r + '0');
+			}
+			if ((r >= 10) && (r != 0))
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(r / 10 + '0');
+				_putchar(r % 10 + '0');
+			}
 		}
-		b = 0;
-		_putchar('\n');
-		a++;
+	_putchar('\n');
 	}
 }
+

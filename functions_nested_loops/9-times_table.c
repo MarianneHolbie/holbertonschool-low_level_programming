f01e2f9fdf32f27c64eebc5dc1e13c265e1d1a3f
Arrue Marianne <5608@holbertonstudents.com>
2022-10-07 12:48:01 -0700

    the real table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -10,16 +10,21 @@ void times_table(void)
 {
 	int a, b, r;
 
+	/* loop for line */
 	for (a = 0; a < 10; a++)
 	{
+		/* loop for culumn */
 		for (b = 0; b < 10; b++)
 		{
+			/* the product */
 			r = a * b;
-			if (r == 0)
+
+			if (b == 0)
 			{
 				_putchar(r + '0');
 			}
-			if ((r < 10) && (r != 0))
+
+			if ((r < 10) && (b != 0))
 			{
 				_putchar(',');
 				_putchar(' ');

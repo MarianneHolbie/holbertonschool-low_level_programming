54faa2e6f61910f9caedabb083d42ae3f1cca2c0
Arrue Marianne <5608@holbertonstudents.com>
2022-10-11 06:36:22 -0700

    overwrite string

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -11,11 +11,15 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
+	int i, j;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; (i < n) && (*(src + i) != '\0'); i++)
 	{
 		*(dest + i) = *(src + i);
 	}
+
+	for (j = i; j < n; j++)
+		*(dest + j) = '\0';
+
 	return (dest);
 }

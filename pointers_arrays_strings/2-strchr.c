6f939546348985eebcb7fba624cafca72ccbd80e
Arrue Marianne <5608@holbertonstudents.com>
2022-10-13 11:39:53 -0700

    bugfix case of c = \0

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -12,11 +12,17 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
+	/* read the string */
 	for (i = 0; (*(s + i)) != '\0'; i++)
 	{
+		/* if the char in s[i] is c, return the end of the string */
 		if (*(s + i) == c)
 		return (s + i);
 	}
+
+	if (*(s + 1) == '\0')
+		return (s + 1);
+
 	return ('\0');
 }
 

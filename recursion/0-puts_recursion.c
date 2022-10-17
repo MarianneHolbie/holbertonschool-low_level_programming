5aca95aebd15543297cca979badb279626b4a1d0
Arrue Marianne <5608@holbertonstudents.com>
2022-10-17 01:42:46 -0700

    first recursion

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+void _puts_recursion(char *s)
+{
+	int i = 0;
+
+	if (*(s + i) == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	else
+	{
+		_putchar(*(s + i));
+		i++;
+		_puts_recursion(s + i);
+	}
+}
---
+++ recursion/main.h
@@ -0,0 +1,6 @@
+#include <unistd.h>
+
+/* function print _putchar */
+int _putchar(char);
+/* function that prints a string by recursion */
+void _puts_recursion(char *s);

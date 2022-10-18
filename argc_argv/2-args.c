6f1e157ea91b851f8cb7981045b6eaf87d6c5a39
Arrue Marianne <5608@holbertonstudents.com>
2022-10-18 03:01:15 -0700

    prints all arguments & name

---
+++ argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main- function that print all arguments receives
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+
+	return (0);
+}
+

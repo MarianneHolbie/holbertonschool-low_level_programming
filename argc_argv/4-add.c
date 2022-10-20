166700d5936eaafd333fd1a8f183bdda8297ea23
Arrue Marianne <5608@holbertonstudents.com>
2022-10-20 00:59:36 -0700

    bugfix test is number and not caracter

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -13,27 +13,35 @@
 
 int main(int argc, char *argv[])
 {
-	int result = 0, i;
+	int result = 0, x, y, i;
 
 	/* sum if minimum two element */
 	if (argc < 3)
 		printf("0\n");
 
+	/* loop to test if is number et not character */
+	for ( x = 1; x < argc; x++)
+	{
+		for (y = 0; argv[x][y] != '\0'; y++)
+		{
+			if (argv[x][y] <= 57 && argv[x][y] >= 48)
+			{
+				continue;
+			}
+			else
+			{
+				printf("Error\n");
+				return(1);
+			}
+		}
+	}
+
 	/* loop to read argument without name function */
 	for (i = 1; i <= argc - 1; i++)
 	{
-		/* test if character is not a digit */
-		if (!isdigit(*argv[i]))
-		{
-			printf("Error\n");
-			return (1);
-		}
-		/* sum each argument */
-		else
-		{
-			result = result + atoi(argv[i]);
-		}
+		result = result + atoi(argv[i]);
 	}
 	printf("%d\n", result);
 	return (0);
 }
+	

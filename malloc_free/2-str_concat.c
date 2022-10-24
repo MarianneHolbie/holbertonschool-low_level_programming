226046333285d87e1cbd58aa25c8c32109dc2131
Arrue Marianne <5608@holbertonstudents.com>
2022-10-24 05:01:49 -0700

    grr

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -31,11 +31,11 @@ char *str_concat(char *s1, char *s2)
 	unsigned int i, len1 = 0, len2 = 0;
 	char *ar;
 
-	/* if chain is NULL treat as an empty string */
+	/* if chain is NULL treat as an empty string 
 	if (s1 == NULL)
 		len1 = 0;
 	if (s2 == NULL)
-		len2 = 0;
+		len2 = 0; */
 
 	if (len1 != 0)
 		len1 = _strlen(s1);

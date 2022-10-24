027aa7a3bde7eee5aa1a7f0c7998b8798a3270ff
Arrue Marianne <5608@holbertonstudents.com>
2022-10-24 05:20:14 -0700

    grr

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -31,12 +31,6 @@ char *str_concat(char *s1, char *s2)
 	unsigned int i, len1 = 0, len2 = 0;
 	char *ar;
 
-	/* if chain is NULL treat as an empty string
-	if (s1 == NULL)
-		len1 = 0;
-	if (s2 == NULL)
-		len2 = 0; */
-
 	if (s1 == 0)
 		len1 = 0;
 	else
@@ -46,11 +40,6 @@ char *str_concat(char *s1, char *s2)
 		len2 = 0;
 	else
 		len2 = _strlen(s2);
-
-	/*if (len1 != 0)
-		len1 = _strlen(s1);
-	if (len2 != 0)
-		len2 = _strlen(s2);*/
 
 	ar = malloc((len1 + len2 + 1) * sizeof(char));
 
@@ -64,7 +53,7 @@ char *str_concat(char *s1, char *s2)
 	}
 
 	/* input S2 en ar */
-	for (i = 0; i < len2 + 1; i++)
+	for (i = 0; i < len2 ; i++)
 	{
 		ar[len1] = s2[i];
 		len1++;

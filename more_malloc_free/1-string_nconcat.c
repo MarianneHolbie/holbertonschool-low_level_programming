ed6acdf0fd94f94388943d285ef6f2e994400971
Arrue Marianne <5608@holbertonstudents.com>
2022-10-25 04:37:53 -0700

    bugfix

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -26,7 +26,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	else
 		len2 = strlen(s2);
 
-	p = malloc((len1 + n + 1) * sizeof(char));
+	p = malloc((len1 + n) * sizeof(char));
 	if (p == NULL)
 	{
 		free(p);

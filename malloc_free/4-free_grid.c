8fee91e094f41b0191a99895ba32c50958373015
Arrue Marianne <5608@holbertonstudents.com>
2022-10-25 00:45:23 -0700

    free 2d array previously create

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_grid- function that frees a 2 dimensional grid previously created
+ * @grid: the 2 dimensional array
+ * @height: height of the array
+ *
+ * Return: void
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
--- malloc_free/main.h
+++ malloc_free/main.h
@@ -11,5 +11,7 @@ char *str_concat(char *s1, char *s2);
 int _strlen(char *s);
 /* function that returns a pointe to a 2 dimensional array of integers */
 int **alloc_grid(int width, int height);
+/* function that frees a 2 dimensional grid previously created */
+void free_grid(int **grid, int height);
 
 #endif

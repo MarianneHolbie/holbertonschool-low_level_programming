e8f9ac44bb0b97820e8ec23ac8e84bb63384f843
Arrue Marianne <5608@holbertonstudents.com>
2022-10-25 04:36:05 -0700

    bugfix len2

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -37,7 +37,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (n >= len2)
 	{
-		for (i = 0; i < len2 - 1; i++)
+		for (i = 0; i < len2; i++)
 		{
 			p[i] = s2[i];
 			len1++;
@@ -45,7 +45,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	else
 	{
-		for (i = 0; i <= n - 1; i++)
+		for (i = 0; i <= n; i++)
 		{
 			p[len1] = s2[i];
 			len1++;

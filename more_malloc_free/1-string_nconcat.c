d02955a527eff9776acf6988d035ba488200d99b
Arrue Marianne <5608@holbertonstudents.com>
2022-10-25 03:25:53 -0700

    string concatenate with n byte

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,59 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * string_nconcat- concatenate one string and n byte of the other
+ * @s1: first string
+ * @s2: second string
+ * @n: number of bytes of s2
+ *
+ * Return: pointer
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i, len1 = 0, len2 = 0;
+	char *p;
+
+	if (s1 == 0)
+		len1 = 0;
+	else
+		len1 = strlen(s1);
+
+	if (s2 == 0)
+		len2 = 0;
+	else
+		len2 = strlen(s2);
+
+	p = malloc((len1 + n) * sizeof(char));
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; i < len1; i++)
+		p[i] = s1[i];
+
+	if (n >= len2)
+	{
+		for (i = 0; i < len2; i++)
+		{
+			p[i] = s2[i];
+			len1++;
+		}
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			p[len1] = s2[i];
+			len1++;
+		}
+		p[len1] = '\0';
+	}
+	return (p);
+}
+
+
+
+
--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -3,6 +3,7 @@
 
 /* function  that allocates memory using malloc */
 void *malloc_checked(unsigned int b);
-
+/* function that concatenates two strings but only n bytes of the second */
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 
 #endif

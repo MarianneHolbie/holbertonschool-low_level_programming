4e1610821ef68eab7ddbee8370f48310373ed7f9
Arrue Marianne <5608@holbertonstudents.com>
2022-10-31 01:26:17 -0700

    first data structure dog

---
+++ structures_typedef/README.md
@@ -0,0 +1,8 @@
+PROJECT STRUCTURES,TYPEDEF
+
+dog.h
+1-init_dog.c
+2-print_dog.c
+dog.h
+4-new_dog.c
+5-free_dog.c
---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog- ID dog
+ * @name: name of dog
+ * @age: age of the dog
+ * @owner: name of owner
+ *
+ * Description: name and age of dog + name of the owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif

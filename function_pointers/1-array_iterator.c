b28d66929508e0c47c7647a7e958d1718285f999
Arrue Marianne <5608@holbertonstudents.com>
2022-11-01 01:53:58 -0700

    function to use a function on eache element of an array

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stddef.h>
+
+/**
+ * array_iterator- function that execute function on eache element of an array
+ * @array: array of element
+ * @size: length of the array
+ * @action: pointer to function to use
+ *
+ * Return:
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,9 +1,12 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
+#include <stddef.h>
 
 /* function putchar */
 int _putchar(char);
 /* function that print a name */
 void print_name(char *name, void (*f)(char *));
+/* function that execute function on each element of an array */
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif

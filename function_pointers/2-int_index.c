1965b9e8b72b96e741e36a041a96ac994a569501
Arrue Marianne <5608@holbertonstudents.com>
2022-11-01 02:18:11 -0700

    index searche

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,30 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * int_index- function that searches integer with different function called
+ * @array: array to search element corresponding
+ * @size: size of array
+ * @cmp: pointer to function called
+ *
+ * Return: position of element, or -1 if no element or size <=0
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	/* case of size <= 0 */
+	if (size <= 0)
+		return (-1);
+
+	/* read each element of the array */
+	for (i = 0; i < size; i++)
+	{
+		/* apply function */
+		if (cmp(array[i]))
+			return (i);
+	}
+
+	return (-1);
+}
--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -8,5 +8,7 @@ int _putchar(char);
 void print_name(char *name, void (*f)(char *));
 /* function that execute function on each element of an array */
 void array_iterator(int *array, size_t size, void (*action)(int));
+/* function that searches for an integer */
+int int_index(int *array, int size, int (*cmp)(int));
 
 #endif

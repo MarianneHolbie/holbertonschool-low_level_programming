d3543637d907bdfa18d767f8a09ea72caea8941b
Arrue Marianne <5608@holbertonstudents.com>
2022-11-02 02:16:48 -0700

    print number with separator

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,39 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_numbers- function that prints number
+ * @separator: string to be printed between numbers
+ * @n: count of numbers
+ *
+ * Return: NULL or number separate by separator
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	int nb = 0;
+
+	va_list pnb;
+
+	if (separator == NULL)
+		exit(0);
+
+	va_start(pnb, n);
+
+	for (i = 0; i < n; i++)
+	{
+		nb = va_arg(pnb, int);
+
+		if (i != (n - 1))
+		{
+			printf("%d%s", nb, separator);
+		}
+	}
+	printf("%d", nb);
+
+	va_end(pnb);
+	printf("\n");
+}
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -3,5 +3,7 @@
 
 /* function that returns the sum of all its parameters */
 int sum_them_all(const unsigned int n, ...);
+/* function that prints numbers */
+void print_numbers(const char *separator, const unsigned int n, ...);
 
 #endif

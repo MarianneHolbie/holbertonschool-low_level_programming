7ec89fc1319638c416def69bfe3e653ed44fbac0
Arrue Marianne <5608@holbertonstudents.com>
2022-11-04 00:57:03 -0700

    first try

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,88 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * pr_c- function that print char
+ * @tall: char to print
+ */
+
+void pr_c(va_list tall)
+{
+	printf("%c", va_arg(tall, int));
+}
+
+/**
+ * pr_i- function that print an integer
+ * @tall: int to print
+ */
+
+void pr_i(va_list tall)
+{
+	printf("%d", va_arg(tall, int));
+}
+
+/**
+ * pr_f- function that print float
+ * @tall: float to print
+ */
+
+void pr_f(va_list tall)
+{
+	printf("%f", va_arg(tall, double));
+}
+
+/**
+ * pr_s- function to print string
+ * @tall: string
+ */
+
+void pr_s(va_list tall)
+{
+	char *txt;
+
+	txt = va_arg(tall, char *);
+
+	if (txt == NULL)
+		txt = "(nil)";
+	printf("%s", txt);
+}
+
+/**
+ * print_all- function that printf good type of arg
+ * @format: type of arg to print
+ *
+ */
+
+void print_all(const char * const format, ...)
+{
+	int i = 0;
+
+	/* new entry of struct pr */
+	pr_arg tall[] = {
+		{"c", pr_c},
+		{"i", pr_i},
+		{"f", pr_f},
+		{"s", pr_s},
+		{NULL, NULL},
+	};
+
+	/* pointer to the argument list */
+	va_list to_pr;
+
+	va_start(to_pr, format);
+
+	while (*format != 0)
+	{
+		while (i < 5)
+		{
+			if (*format == *tall[i].t_arg)
+				tall[i].f(to_pr);
+			i++;
+		}
+		i = 0;
+	}
+
+	va_end(to_pr);
+	printf("\n");
+}
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,12 +1,34 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
+#include <stdarg.h>
+
 /* function that returns the sum of all its parameters */
 int sum_them_all(const unsigned int n, ...);
 /* function that prints numbers */
 void print_numbers(const char *separator, const unsigned int n, ...);
 /* function that prints strings */
 void print_strings(const char *separator, const unsigned int n, ...);
+/* function that prints anything */
+void print_all(const char * const format, ...);
 
+/* function that print different format */
+void pr_c(va_list tall);
+void pr_i(va_list tall);
+void pr_f(va_list tall);
+void pr_s(va_list tall);
+
+/**
+ * struct pr_arg- Struct pr_arg
+ *
+ * @targ: type of character
+ * @f: function associated
+ */
+
+typedef struct pr
+{
+	char *t_arg;
+	void (*f)(va_list);
+} pr_arg;
 
 #endif

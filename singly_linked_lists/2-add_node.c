0db94cacbddb215c6805214fa04b168526f6d822
Arrue Marianne <5608@holbertonstudents.com>
2022-11-14 04:53:53 -0800

    new element in the head of linked list

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <string.h>
+
+/**
+ * add_node- add new node at the beginning
+ * @head: first node
+ * @str: value of new node
+ *
+ * Return: address of new node or Null if it failed
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	char *copy; /* copy of string */
+	list_t *new_node; /* new element */
+
+	copy = strdup(str); /* copy the string */
+
+	/* allocate node */
+	new_node = malloc(sizeof(list_t));
+
+	/* put new data */
+	new_node->str = copy;
+	new_node->len = strlen(copy); /* length of string */
+
+	/* make next of new node as head */
+	new_node->next = (*head);
+
+	/* move the head to point to the new node */
+	(*head) = new_node;
+
+	return (new_node);
+	free(copy);
+}

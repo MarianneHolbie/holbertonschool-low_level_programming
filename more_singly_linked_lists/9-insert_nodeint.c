ff230ec15b7defe49821c429f82718e49b268e40
Arrue Marianne <5608@holbertonstudents.com>
2022-11-15 05:20:33 -0800

    bugfix error to address next if node at the beginning

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -33,7 +33,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (new_node);
 	}
 
-	for (i = 0; i < idx - 1; i++) /* go to idx - 1 node */
+	for (i = 0; i < (idx - 1); i++) /* go to idx - 1 node */
 	{
 		if (copy_head != NULL) /* if it not the last node */
 		{

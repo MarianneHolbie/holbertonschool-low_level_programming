58c0bc73af0b4999f1463a2466409e65b95af1ce
Arrue Marianne <5608@holbertonstudents.com>
2022-11-15 05:31:52 -0800

    bugfix if new_node at beginning head copy_head is second element

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -28,7 +28,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (idx == 0) /* add at the beginning */
 	{
-		new_node->next = copy_head->next; /* next new_node is head */
+		new_node->next = copy_head; /* next new_node is head */
 		*head = new_node; /* new_node became head */
 		return (new_node);
 	}

146eb03cef9c50470ab43cf89563c580dfd97941
Arrue Marianne <5608@holbertonstudents.com>
2022-11-21 05:12:51 -0800

    add O_TRUNC flag if file exist

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	/* create and open file name filenam with rw_______ right */
-	fildes = open(filename, O_CREAT | O_RDWR, 0600);
+	fildes = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fildes == -1)
 		return (-1);
 

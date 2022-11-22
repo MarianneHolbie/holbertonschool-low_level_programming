f59e3bc13d4b66f3e4dd234f815c576952fe8729
Arrue Marianne <5608@holbertonstudents.com>
2022-11-22 01:23:39 -0800

    initialize program

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_dlistint- function that frees a dlistint_t
+ * @head : pointer to head
+ *
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+
+	while (head == NULL)
+	{
+		temp = head->next;
+		temp = head->prev;
+		free(head);
+		head = temp;
+	}
+	free(temp);
+}
+
--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -30,5 +30,7 @@ size_t dlistint_len(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 /* add new node at the end of dlistint_t */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+/* frees a dlistint_t list */
+void free_dlistint(dlistint_t *head);
 
 #endif

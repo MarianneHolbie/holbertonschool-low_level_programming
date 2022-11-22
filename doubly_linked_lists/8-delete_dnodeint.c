0b1665023db630d2b4278c5d478c3cc03e83a825
Arrue Marianne <5608@holbertonstudents.com>
2022-11-22 05:47:27 -0800

    try another algorithm with if statement

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -10,43 +10,32 @@
 
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *copy_head, *temp;
+	dlistint_t *copy_head;
 	unsigned int count = 0;
 
 	copy_head = (*head);
 
+	if (*head == NULL)
+		return (-1);
+
+	for (count = 0; copy_head != NULL && count < index; count++)
+	{
+		copy_head = copy_head->next;
+	}
 	if (copy_head == NULL)
 		return (-1);
 
-	if (index == 0)
-	{
-		if ((*head)->next != NULL)
-		{
-			(*head) = (*head)->next;
-			free(copy_head);
-			return (1);
-		}
-		else
-		{
-			free(*head);
-			*head = NULL;
-			return (1);
-		}
-	}
-		while (head && count < index)
-		{
-			temp = copy_head;
-			if (copy_head->next)
-			{
-				copy_head = copy_head->next;
-				temp->next->prev = temp->prev;
-				count++;
-			}
-			else
-				return (-1);
-		}
+	if (*head == NULL || copy_head == NULL)
+		return (-1);
 
-		temp->next = copy_head->next;
-		free(copy_head);
-		return (1);
+	if (*head == copy_head)
+		*head = copy_head->next;
+
+	if (copy_head->next != NULL)
+		copy_head->next->prev = copy_head->prev;
+
+	if (copy_head->prev != NULL)
+		copy_head->prev->next = copy_head->next;
+	free(copy_head);
+	return (1);
 }

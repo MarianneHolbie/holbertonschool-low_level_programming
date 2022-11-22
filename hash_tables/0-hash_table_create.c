232f608c652b4b89d6f058c728b8f1873676fdf1
Arrue Marianne <5608@holbertonstudents.com>
2022-11-22 11:25:51 -0800

    add malloc to hash_node_t array item

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -9,6 +9,8 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	long unsigned int i;
+
 	/* create table type struct */
 	hash_table_t *table;
 
@@ -19,6 +21,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	table->size = size;
+	table->array = malloc(size * sizeof(hash_node_t));
+
+	for (i = 0; i < table->size; i++)
+		table->array[i] = NULL;
 
 	return (table);
 }

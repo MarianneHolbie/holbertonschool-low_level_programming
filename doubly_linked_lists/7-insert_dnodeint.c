e31a87d45bf2a6607b1c6a802aaf408490687828
Arrue Marianne <5608@holbertonstudents.com>
2022-11-22 03:14:05 -0800

    fix case idx == 0

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -15,16 +15,27 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *temp = *h;
 	unsigned int i;
 
+	if (h == NULL)
+		return (NULL);
+
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 	{
 		free(new_node);
 		return (NULL);
 	}
-
 	new_node->n = n;
 
-	for (i = 0; i < idx + 1; i++)
+	if (idx == 0)
+	{
+		new_node->next = temp;
+		new_node->prev = NULL;
+		temp->prev = new_node;
+		temp = new_node;
+		*h = temp;
+		return (new_node);
+	}
+	for (i = 0; i < idx; i++)
 	{
 		if (i == idx - 1)
 		{
@@ -34,11 +45,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			i++;
 		}
 		if (i == idx)
-		{
 			temp->prev = new_node;
-		}
 		temp = temp->next;
 	}
-
 	return (new_node);
 }

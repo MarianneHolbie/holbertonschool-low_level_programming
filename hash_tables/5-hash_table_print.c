220049d62c14d136e9ccb8771c594ee2982b42e3
Arrue Marianne <5608@holbertonstudents.com>
2022-11-24 12:42:59 -0800

    bugfix problem of separator beetween two key value printed

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -9,6 +9,10 @@
 void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i = 0;
+	int separator = 0;
+
+	if (ht == NULL)
+		exit(0);
 
 	printf("{");
 	for (i = 0; i < ht->size; i++)
@@ -16,10 +20,11 @@ void hash_table_print(const hash_table_t *ht)
 		if (ht->array[i])
 		{
 			printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
+			separator = 1;
 			if (ht->array[i]->next != NULL)
 				printf(", ");
 		}
-		if (ht->array[i + 1] != NULL)
+		if (separator == 1 && ht->array[i + 1] != NULL)
 			printf(", ");
 	}
 	printf("}\n");

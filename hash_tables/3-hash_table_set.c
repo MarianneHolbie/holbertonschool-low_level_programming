92f4dc54169c5e3503fc26d41ea4565ed12a1010
Arrue Marianne <5608@holbertonstudents.com>
2022-11-24 04:30:39 -0800

    restore

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -40,7 +40,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		if (strcmp(ht->array[index]->key, new_item->key) == 0)
 		{
-			free(ht->array[index]->key);
 			ht->array[index]->value = new_item->value;
 			return (1);
 		}

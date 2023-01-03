60569a51cdb2cb4df2e3e8bd9cc584afccc4dcf6
Arrue Marianne <5608@holbertonstudents.com>
2023-01-03 12:02:52 -0800

    initialized linear search algo

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,30 @@
+#include "search_algos.h"
+
+/**
+ * linear_search- searches value in array using linear search algo
+ * @array: array to search
+ * @size: size of the array
+ * @value: searched value
+ *
+ * Return: index of searched value found or -1
+ */
+
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t index = -1;
+	size_t index_found = 0;
+
+	for (index = 0; index < size; index++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", index, array[index]);
+		if (array[index] == value)
+		{
+			index_found = index;
+			break;
+		}
+		else
+			index_found = -1;
+	}
+	return (index_found);
+}

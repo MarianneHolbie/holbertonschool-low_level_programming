9e1b806d10c558a5659a7de500c556fcc843908e
Arrue Marianne <5608@holbertonstudents.com>
2023-01-04 00:03:52 -0800

    initialize binary search algo

---
+++ search_algorithms/1-binary.c
@@ -0,0 +1,71 @@
+#include "search_algos.h"
+
+
+/**
+ * print_array - Prints an array of integers
+ *
+ * @array: The array to be printed
+ * @low: low index in the array
+ * @high: high index in the array
+ */
+void print_array(const int *array, size_t low, size_t high)
+{
+	size_t i;
+
+	i = low;
+	while (array && i < high)
+	{
+		if (i > 0)
+			printf(", ");
+		printf("%d", array[i]);
+		++i;
+	}
+	printf("\n");
+}
+
+
+/**
+ * binary_search- searches in sorted array with binary search algo
+ * @array: sorted array
+ * @size: size of array
+ * @value: searched value
+ *
+ * Return: index where value found or -1
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+	size_t index;
+	size_t middle;
+	int low, high;
+
+	if (array == NULL || !size)
+		return (-1);
+
+	low = 0;
+	high = size - 1;
+
+	while (low != high)
+	{
+		printf("Searching in array:");
+		print_array(array, low, high);
+		middle = (low + high) / 2;
+
+		if (value == array[middle])
+		{
+			index = middle;
+			return (index);
+		}
+		if (value < array[middle])
+		{
+			high = middle - 1;
+			size = (middle - 1) - low;
+		}
+		if (value > array[middle])
+		{
+			low = middle + 1;
+			size = high - (middle + 1);
+		}
+	}
+	return (-1);
+}
--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -6,5 +6,10 @@
 
 /* function of search */
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+
+/* helper function*/
+void print_array(const int *array, size_t low, size_t high);
 
 #endif

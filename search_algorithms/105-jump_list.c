5a41cc3d488e6ef4e1e417dcf3b8f0b9fe53946c
Arrue Marianne <5608@holbertonstudents.com>
2023-01-06 02:58:29 -0800

    add function print_list in the file

--- search_algorithms/105-jump_list.c
+++ search_algorithms/105-jump_list.c
@@ -1,5 +1,22 @@
 #include "search_algos.h"
 
+
+/**
+ * print_list - Prints the content of a listint_t
+ *
+ * @list: Pointer to the head of the list
+ */
+void print_list(const listint_t *list)
+{
+	printf("List :\n");
+	while (list)
+	{
+		printf("Index[%lu] = [%d]\n", list->index, list->n);
+		list = list->next;
+	}
+	printf("\n");
+}
+
 /**
  * jump_list- search value in sorted list using jump search algo
  * @list: single sorted linked list

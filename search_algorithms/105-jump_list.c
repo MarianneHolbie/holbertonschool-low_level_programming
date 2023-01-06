04246ba49d42d64f43a5ab5cdcc5258283d6af82
Arrue Marianne <5608@holbertonstudents.com>
2023-01-06 02:57:26 -0800

    initialize jump search algo for linked list

---
+++ search_algorithms/105-jump_list.c
@@ -0,0 +1,59 @@
+#include "search_algos.h"
+
+/**
+ * jump_list- search value in sorted list using jump search algo
+ * @list: single sorted linked list
+ * @size: size of linked list
+ * @value: searched value
+ * 
+ * Return: index of searched value or NULL
+*/
+
+listint_t *jump_list(listint_t *list, size_t size, int value)
+{
+    listint_t *temp;
+    size_t jump, index;
+
+    if (list == NULL || !size )
+        return (NULL);
+
+    print_list(list);
+
+    index = 0;
+    jump = sqrt(size);
+
+    temp = list;
+
+	/* List with 1 element*/
+	if (size == 0 && temp->n != value)
+		return (NULL);
+
+	while (temp->n < value && index < size)
+	{
+		index = index + jump;
+		list = temp;
+		while (temp->index < index)
+		{
+			if (temp->next != NULL)
+				temp = temp->next;
+			else
+				break;
+		}
+		printf("Value checked at index [%ld] = [%d]\n", temp->index, temp->n);
+	}	
+
+	printf("Value found between indexes [%ld] and [%ld]\n", list->index, temp->index);
+
+	/* only on good part make linear search*/
+    while (list->n <= value && list->index <= temp->index)
+	{
+		printf("Value checked at index [%ld] = [%d]\n", list->index, list->n);
+		if (list->n == value)
+			return (list);
+		if (list->next == NULL)
+			return (NULL);
+		list = list->next;
+	}
+
+	return (NULL);
+}

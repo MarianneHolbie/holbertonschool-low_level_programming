ca1f4eb2e088393463723e7896cbfb6bc35d6443
Arrue Marianne <5608@holbertonstudents.com>
2023-03-02 13:00:29 -0800

    fix betty

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -3,10 +3,11 @@
 void before_main() __attribute__((constructor));
 
 /**
- * before- prints all the element of a list
+ * before_main- prints all the element of a list
  */
 
 void before_main()
 {
-    printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n
+			I bore my house upon my back!\n");
 }
